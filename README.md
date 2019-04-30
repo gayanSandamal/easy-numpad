12fe549dd0a11854cb81f73b77c70b8d3060227d
unknown <r.damico@reply.it>
2019-04-30 16:13:28 +0200

    Working to remove JQuery, added a new feature

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
 # easy-numpad
-jQuery based soft number pad which is developed use and customize easily
+A Fork form an original project by [Gayan Sandamal](https://github.com/gayanSandamal/easy-numpad).
 
-<h3><a href="https://plugins.nayague.com/easy-numpad/" target="_blank">Live example<a/></h3>
+Currently it still uses JQuery, but I'm modifying the code to use only Javascript without external frameworks, in order to improve compatibility with all browsers and improve performance.
 
-* Single element usage.
+**Added a new feature**, when selecting an input field already valued, the value is also shown in the easy-numpad that opens.
 
-1. Add "easy-get" class name where you click to get soft number pad.
-    example - <div class="easy-get"></div>
 
-2. Add "easy-put" class name where you want to show result.
-    example - <input type="text" class="easy-put"/>
 
-* * Mulitple element usage.
+# How to use
+Simple add this code where you need, every time you need in your HTML page
 
-1. Call below function in any function with any parameter you want.
-    show_easy_numpad();
+```
+<input id="yourElementId" type="number" readonly="true" onclick="show_easy_numpad(this);" />
+```
 
-    example -  
+# Try it
 
-    function quantity(x, y, z) {
-        show_easy_numpad(x);
-    }
+[Live example of use it](https://bobboteck.github.io/easy-numpad/index.html)
--- js/easy-numpad.js
+++ js/easy-numpad.js
@@ -1,60 +1,63 @@
-$(document).ready(function () {
-    $('.easy-get').on('click', () => {
-        show_easy_numpad();
-    });
-});
+let _outputID = "";
 
-function show_easy_numpad() {
-    var easy_numpad = `
-        <div class="easy-numpad-frame" id="easy-numpad-frame">
-            <div class="easy-numpad-container">
-                <div class="easy-numpad-output-container">
-                    <p class="easy-numpad-output" id="easy-numpad-output"></p>
-                </div>
-                <div class="easy-numpad-number-container">
-                    <table>
-                        <tr>
-                            <td><a href="7" onclick="easynum()">7</a></td>
-                            <td><a href="8" onclick="easynum()">8</a></td>
-                            <td><a href="9" onclick="easynum()">9</a></td>
-                            <td><a href="Del" class="del" id="del" onclick="easy_numpad_del()">Del</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="4" onclick="easynum()">4</a></td>
-                            <td><a href="5" onclick="easynum()">5</a></td>
-                            <td><a href="6" onclick="easynum()">6</a></td>
-                            <td><a href="Clear" class="clear" id="clear" onclick="easy_numpad_clear()">Clear</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="1" onclick="easynum()">1</a></td>
-                            <td><a href="2" onclick="easynum()">2</a></td>
-                            <td><a href="3" onclick="easynum()">3</a></td>
-                            <td><a href="Cancel" class="cancel" id="cancel" onclick="easy_numpad_cancel()">Cancel</a></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" onclick="easynum()"><a href="0">0</a></td>
-                            <td onclick="easynum()"><a href=".">.</a></td>
-                            <td><a href="Done" class="done" id="done" onclick="easy_numpad_done()">Done</a></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+function show_easy_numpad(thisElement)
+{
+    let easy_numpad = document.createElement("div");
+    easy_numpad.id = "easy-numpad-frame";
+    easy_numpad.className = "easy-numpad-frame";
+    easy_numpad.innerHTML = `
+    <div class="easy-numpad-container">
+        <div class="easy-numpad-output-container">
+            <p class="easy-numpad-output" id="easy-numpad-output"></p>
         </div>
+        <div class="easy-numpad-number-container">
+            <table>
+                <tr>
+                    <td><a href="7" onclick="easynum()">7</a></td>
+                    <td><a href="8" onclick="easynum()">8</a></td>
+                    <td><a href="9" onclick="easynum()">9</a></td>
+                    <td><a href="Del" class="del" id="del" onclick="easy_numpad_del()">Del</a></td>
+                </tr>
+                <tr>
+                    <td><a href="4" onclick="easynum()">4</a></td>
+                    <td><a href="5" onclick="easynum()">5</a></td>
+                    <td><a href="6" onclick="easynum()">6</a></td>
+                    <td><a href="Clear" class="clear" id="clear" onclick="easy_numpad_clear()">Clear</a></td>
+                </tr>
+                <tr>
+                    <td><a href="1" onclick="easynum()">1</a></td>
+                    <td><a href="2" onclick="easynum()">2</a></td>
+                    <td><a href="3" onclick="easynum()">3</a></td>
+                    <td><a href="Cancel" class="cancel" id="cancel" onclick="easy_numpad_cancel()">Cancel</a></td>
+                </tr>
+                <tr>
+                    <td colspan="2" onclick="easynum()"><a href="0">0</a></td>
+                    <td onclick="easynum()"><a href=".">.</a></td>
+                    <td><a href="Done" class="done" id="done" onclick="easy_numpad_done()">Done</a></td>
+                </tr>
+            </table>
+        </div>
+    </div>
     `;
-    $('body').append(easy_numpad);
+
+    document.getElementsByTagName('body')[0].appendChild(easy_numpad);
+    _outputID = thisElement.id;
+    document.getElementById("easy-numpad-output").innerText = thisElement.value;
 }
 
-function easy_numpad_close() {
-    $('#easy-numpad-frame').remove();
+function easy_numpad_close()
+{
+    let elementToRemove = document.querySelectorAll("div.easy-numpad-frame")[0];
+    elementToRemove.parentNode.removeChild(elementToRemove);
 }
 
 function easynum() {
     event.preventDefault();
 
-    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+    /*navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
     if (navigator.vibrate) {
         navigator.vibrate(60);
-    }
+    }*/
 
     var easy_num_button = $(event.target);
     var easy_num_value = easy_num_button.text();
@@ -77,6 +80,6 @@ function easy_numpad_cancel() {
 function easy_numpad_done() {
     event.preventDefault();
     var easy_numpad_output_val = $('#easy-numpad-output').text();
-    $('.easy-put').val(easy_numpad_output_val);
+    document.getElementById(_outputID).value=easy_numpad_output_val;
     easy_numpad_close();
 }
